c703f181f90b9ef36b2746700ecdb4984e9645b0
Ross Cairns <ross@electricglen.com>
2018-02-21 21:52:38 +0000

    Added conditional downloading

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 mkdir data
 cd data
-wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+wget -nc http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 mkdir bin
-wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+wget -nc http://msvocds.blob.core.windows.net/coco2014/train2014.zip
 unzip train2014.zip
